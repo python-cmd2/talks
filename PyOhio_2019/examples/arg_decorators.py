85940412bdad11b56648834a9f17ae6968d4c469
Kevin Van Brunt <kmvanbrunt@gmail.com>
2019-07-22 01:32:23 -0400

    Replaced an example with something that is cross-platform

--- PyOhio_2019/examples/arg_decorators.py
+++ PyOhio_2019/examples/arg_decorators.py
@@ -12,34 +12,37 @@ class ArgparsingApp(cmd2.Cmd):
         super().__init__(use_ipython=True)
         self.intro = 'cmd2 has awesome decorators to make it easy to use Argparse to parse command arguments'
 
-    cat_parser = cmd2.Cmd2ArgumentParser(description='concatenate and print files', epilog='See "man cat"')
-    flag_group = cat_parser.add_mutually_exclusive_group()
-    flag_group.add_argument('-o', '--output_file', help='output written to this file',
-                            completer_method=cmd2.Cmd.path_complete)
-    flag_group.add_argument('-p', '--paged', action='store_true', help='display using a pager')
+    # do_fsize parser
+    fsize_parser = cmd2.Cmd2ArgumentParser(description='Obtain the size of a file')
+    fsize_parser.add_argument('-c', '--comma', action='store_true',
+                              help='add comma for thousands separator',
+                              completer_method=cmd2.Cmd.path_complete)
+    fsize_parser.add_argument('file_path', help='path of file',
+                              completer_method=cmd2.Cmd.path_complete)
 
-    cat_parser.add_argument('files_to_cat', nargs=argparse.ONE_OR_MORE, help='files to concatinate',
-                            completer_method=cmd2.Cmd.path_complete)
+    @cmd2.with_argparser(fsize_parser)
+    def do_fsize(self, args: argparse.Namespace) -> None:
+        """Obtain the size of a file"""
+        expanded_path = os.path.expanduser(args.file_path)
 
-    @cmd2.with_argparser(cat_parser)
-    def do_cat(self, args: argparse.Namespace) -> None:
-        """concatenate and print files"""
-        shell_command = '!cat ' + ' '.join(args.files_to_cat)
-        if args.paged:
-            shell_command += ' | ' + self.pager
-        elif args.output_file:
-            shell_command += ' > {}'.format(os.path.expanduser(args.output_file))
-        self.poutput('Running shell command {!r}'.format(shell_command))
-        self.onecmd_plus_hooks(shell_command, add_to_history=False)
-        self.poutput('')
+        try:
+            size = os.path.getsize(expanded_path)
+        except OSError as ex:
+            self.perror("Error retrieving size: {}".format(ex))
+            return
 
+        if args.comma:
+            size = '{:,}'.format(size)
+        self.poutput('{} bytes'.format(size))
+
+    # do_pow parser
     pow_parser = argparse.ArgumentParser()
     pow_parser.add_argument('base', type=int)
     pow_parser.add_argument('exponent', type=int, choices=range(-5, 6))
 
     @cmd2.with_argparser(pow_parser)
     def do_pow(self, args: argparse.Namespace) -> None:
-        """Raise an integer to a small integer exponent, either positive or negative."""
+        """Raise an integer to a small integer exponent, either positive or negative"""
         self.poutput('{} ** {} == {}'.format(args.base, args.exponent, args.base ** args.exponent))
 
 
